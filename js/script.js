01557fec1e1292074c2399d122e1230b867a89b8
satvik204 <78365606+satvik204@users.noreply.github.com>
2024-10-12 15:20:40 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let menu = document.querySelector("#menu-icon");
+let nav = document.querySelector(".navlist");
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    nav.classList.remove('active');
+}
